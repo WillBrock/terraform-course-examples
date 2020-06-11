9216f3278dff403ce38d29b43808c50e0ff11e5c
Focus <wpbrock@gmail.com>
2020-06-10 22:03:20 -0400

    Update examples

--- dynamic-blocks/main.tf
+++ dynamic-blocks/main.tf
@@ -43,3 +43,28 @@ resource "aws_security_group" "main" {
 		Name = "tuts security group"
 	}
 }
+
+/*
+resource "aws_elastic_beanstalk_application" "tftest" {
+	name        = "tf-test-name"
+	description = "tf-test-desc"
+}
+
+resource "aws_elastic_beanstalk_environment" "tfenvtest" {
+	name                = "tf-test-name"
+	application         = "${aws_elastic_beanstalk_application.tftest.name}"
+	solution_stack_name = "64bit Amazon Linux 2015.03 v2.0.3 running Go 1.4"
+
+	setting {
+		namespace = "aws:ec2:vpc"
+		name      = "VPCId"
+		value     = "vpc-xxxxxxxx"
+	}
+
+	setting {
+		namespace = "aws:ec2:vpc"
+		name      = "Subnets"
+		value     = "subnet-xxxxxxxx"
+	}
+}
+*/
